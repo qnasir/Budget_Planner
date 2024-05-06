dd76f7231dfdcc3887c5b22116e4e0d32c4a184a
qnasir <qnasir575@gmail.com>
2024-05-06 11:33:42 +0530

    Kinde used for authentication

---
+++ budget-planner/utils/KindeConfig.jsx
@@ -0,0 +1,10 @@
+import { KindeSDK } from '@kinde-oss/react-native-sdk-0-7x';
+
+export const client = new KindeSDK(
+    'https://qnasir575.kinde.com', 
+    'exp://192.168.107.157:8081', 
+    '4d0c976a9de7406492ea45fa83379426', 
+    'exp://192.168.107.157:8081'
+);
+
+
