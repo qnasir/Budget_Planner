eaf43ed5f067871289000970eba25cceb91ca55e
qnasir <qnasir575@gmail.com>
2024-05-06 22:59:59 +0530

    Category added

--- budget-planner/utils/Colors.jsx
+++ budget-planner/utils/Colors.jsx
@@ -2,5 +2,11 @@ export default {
     BLACK: '#000',
     PRIMARY: '#8B42FC',
     WHITE: '#fff',
-    GRAY: '#D4D4D4'
+    GRAY: '#D4D4D4',
+    BLUE: '#4F75FE',
+    GREEN: '#13C38B',
+    PURPLE: '9F3CFE',
+    RED: '#FF555D',
+    ORANGE: '#FF7D4F',
+    COLOR_LIST: ['#4F75FE', '#13C38B', '#9F3CFE', '#FF555D', '#FF7D4F']
 }
